96ad282f0832f275d2cc553e5a1462a36751e75f
Your Name <marcos.201314@hotmail.com>
2023-01-06 01:46:44 -0300

    feat: add events handler in javascript

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Selecionar elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEdit = document.querySelector("#cancel-edit-btn");
+
+
+// Funções
+
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputValue = todoInput.value
+
+    if(inputValue){
+        console.log(inputValue);
+    }
+});
